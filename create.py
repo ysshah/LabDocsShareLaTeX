74a45a641988997a1eb24a71b8ccf910b211af38
Yash Shah <yashshah0127@gmail.com>
2017-04-06 14:13:57 -0700

    try again

--- create.py
+++ create.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-cmd = 'git diff --name-only $TRAVIS_COMMIT_RANGE'
+cmd = 'git diff --name-only ' + os.environ['TRAVIS_COMMIT_RANGE']
 
 out = subprocess.check_output(cmd.split()).decode('utf-8')
 print(out)
