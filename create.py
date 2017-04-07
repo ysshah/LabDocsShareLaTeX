21fe1ae007ba270c546793edd74b1a4a837c52a2
Yash Shah <yashshah0127@gmail.com>
2017-04-06 21:06:00 -0700

    test rsync

--- create.py
+++ create.py
@@ -4,6 +4,8 @@ import subprocess
 LABS = ['AFM', 'ATM', 'CO2', 'MNO', 'MOT', 'NMR', 'OPT', 'QIE', 'BMC', 'OTZ',
     'COM', 'MUO', 'GMA', 'BRA', 'RUT', 'HAL', 'LLS', 'NLD', 'JOS', 'SHE']
 
+SITE = '0bc915fe-2126-4798-9621-fdf6aa78ed57'
+
 # Command to install LaTeX
 # install_cmd = ('sudo apt-get update && sudo apt-get install '
 #     '--no-install-recommends texlive-fonts-recommended '
@@ -16,33 +18,43 @@ changed_files = subprocess.check_output(
     diff_cmd.split()).decode('utf-8').split('\n')
 
 
-if 'lab.cls' in changed_files:
-    # If lab.cls is changed, rebuild all PDFs
-    # labs_to_rebuild = LABS
-    labs_to_rebuild = ['OPT']
-else:
-    # Otherwise, only rebuild PDFs that have changed files
-    labs_to_rebuild = set()
-    for file in changed_files:
-        lab = file.split('/')[0]
-        if lab in LABS:
-            labs_to_rebuild.add(lab)
+# if 'lab.cls' in changed_files:
+#     # If lab.cls is changed, rebuild all PDFs
+#     # labs_to_rebuild = LABS
+#     labs_to_rebuild = ['OPT']
+# else:
+#     # Otherwise, only rebuild PDFs that have changed files
+#     labs_to_rebuild = set()
+#     for file in changed_files:
+#         lab = file.split('/')[0]
+#         if lab in LABS:
+#             labs_to_rebuild.add(lab)
 
-labs_to_rebuild = ['OPT']
-if len(labs_to_rebuild) > 0:
+# labs_to_rebuild = ['OPT']
+# if len(labs_to_rebuild) > 0:
 
-    # Make a 'build' directory
-    subprocess.call(['mkdir', 'build'])
+#     # Make a 'build' directory
+#     subprocess.call(['mkdir', 'build'])
 
-    base_dir = os.environ['TRAVIS_BUILD_DIR']
-    for lab in labs_to_rebuild:
-        print 'Rebuilding', lab
-        build_cmd = 'cd {}/{} && pdflatex -output-directory ../build {}.tex'.format(
-            base_dir, lab, lab)
-        subprocess.call(build_cmd, shell=True)
+#     base_dir = os.environ['TRAVIS_BUILD_DIR']
+#     for lab in labs_to_rebuild:
+#         print 'Rebuilding', lab
+#         build_cmd = 'cd {}/{} && pdflatex -output-directory ../build {}.tex'.format(
+#             base_dir, lab, lab)
+#         subprocess.call(build_cmd, shell=True)
 
-    print 'Calling ls'
-    subprocess.call('ls')
+#         sync_cmd = ("rsync -vz --ipv4 --progress -e 'ssh -p 2222' ../build/{}.pdf "
+#             "--temp-dir=~/tmp/ live.{}@appserver.live.{}.drush.in:files/writeups/")
+#         subprocess.call(sync_cmd.format(lab, SITE, SITE), shell=True)
 
-    print 'Calling ls build'
-    subprocess.call(['ls', 'build'])
+#     print 'Calling ls'
+#     subprocess.call('ls')
+
+#     print 'Calling ls build'
+#     subprocess.call(['ls', 'build'])
+
+subprocess.call('touch test.txt', shell=True)
+
+sync_cmd = ("rsync -vz --ipv4 --progress -e 'ssh -p 2222' test.txt "
+        "--temp-dir=~/tmp/ live.{}@appserver.live.{}.drush.in:files/writeups/")
+subprocess.call(sync_cmd.format(SITE, SITE), shell=True)
